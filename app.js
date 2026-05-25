75fba61a6ace19c4f2a8d9e552a4925b676020a6
S-Nrupendra <snrupendra@gmail.com>
2026-05-25 13:53:49 +0530

    Updated Readme, Routing

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const express = require('express')
+const express = require('express');
+const authRoutes = require('./routes/auth.routes');
+
 const app = express();
 
 // Middleware
-app.use(express.json())
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 // Test route
 app.get('/', (req, res)=>{
     res.json({message: 'Server is running'});
 });
 
-module.exports = app
+module.exports = app;

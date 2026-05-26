811885d75c86b51281a5bd0b5245aab49eb33a3d
S-Nrupendra <snrupendra@gmail.com>
2026-05-26 15:48:04 +0530

    generating URL

---
+++ utils/generateCode.js
@@ -0,0 +1,10 @@
+const generateCode = () => {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let code = '';
+  for (let i = 0; i < 6; i++) {
+    code += chars[Math.floor(Math.random() * 62)];
+  }
+  return code;
+};
+
+module.exports = generateCode;
